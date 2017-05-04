b17081ab9df02e6a9751d1c0e15d9810f3001384
Erik Novales <enovales@codehitmen.com>
2017-05-03 20:44:16 -0700

    added support for action buttons

--- src/feltlion/Conversation.fs
+++ src/feltlion/Conversation.fs
@@ -49,13 +49,13 @@ type Dialogue =
                 s = DialogueState.Started
             }
 
-let runDialogue(d: Dialogue, i: string): (Dialogue * string) = 
+let runDialogue(d: Dialogue, i: string): (Dialogue * string * string seq) = 
     match d.s with
     | Started ->
         // pick first available node
         match d.c.startNodes |> Seq.tryHead with
-        | Some(start) -> ({ d with s = ComputerSpeaking; n = Some(start) }, "Going to first node")
-        | _ -> ({ d with s = Terminated }, "[dialogue ended]")
+        | Some(start) -> ({ d with s = ComputerSpeaking; n = Some(start) }, "Going to first node", Seq.empty)
+        | _ -> ({ d with s = Terminated }, "[dialogue ended]", Seq.empty)
     | ComputerSpeaking ->
         match (d.n |> Option.map (fun n -> (n.text, n.next |> Seq.map(fun p -> p.text)))) with
         | Some((t, choices)) ->
@@ -64,15 +64,15 @@ let runDialogue(d: Dialogue, i: string): (Dialogue * string) =
                 String.Join("\n", choices |> Seq.mapi (fun n t -> (n + 1).ToString() + ") " + t))
 
             match choices |> Seq.isEmpty with
-            | true -> ({d with s = Terminated }, t + "\n" + choiceText)
-            | false -> ({ d with s = WaitingForResponse }, t + "\n" + choiceText)
-        | _ -> ({ d with s = Terminated }, "[dialogue ended]")
+            | true -> ({d with s = Terminated }, t + "\n" + choiceText, Seq.empty)
+            | false -> ({ d with s = WaitingForResponse }, t + "\n" + choiceText, choices)
+        | _ -> ({ d with s = Terminated }, "[dialogue ended]", Seq.empty)
     | WaitingForResponse ->
         let choice = Int32.Parse(i)
         let nextOpt = 
             d.n 
             |> Option.bind(fun n -> n.next |> Seq.tryItem(choice - 1))
         match (nextOpt) with
-        | Some(n) -> ({ d with s = ComputerSpeaking; n = n.next |> Seq.tryHead }, n.text)
-        | _ -> ({ d with s = Terminated }, "[dialogue ended]")
-    | Terminated -> (d, "[dialogue ended]")
+        | Some(n) -> ({ d with s = ComputerSpeaking; n = n.next |> Seq.tryHead }, n.text, Seq.empty)
+        | _ -> ({ d with s = Terminated }, "[dialogue ended]", Seq.empty)
+    | Terminated -> (d, "[dialogue ended]", Seq.empty)
--- src/feltlion/feltlion.fs
+++ src/feltlion/feltlion.fs
@@ -3,13 +3,21 @@ module feltlion
 open Argu
 open Conversation
 open FSharp.Data
+open FSharp.Data.JsonExtensions
 open System
 open System.Collections.Concurrent
+open System.Drawing
 open System.Text
 open Suave
 open Suave.Filters
+open Suave.Logging
 open Suave.Operators
 open Suave.Successful
+
+let log = Suave.Logging.Log.create("foo")
+
+let logSimple(s: string) =
+    Message.event Info s |> log.logSimple
 
 type Arguments = 
     | [<Mandatory>] IncomingWebhookUrl of url: string
@@ -50,6 +58,61 @@ type SlackRequest =
             ResponseUrl = get "response_url"
         }
 
+type SlackButtonRequest = 
+    {
+        ChannelId: string option
+        UserId: string option
+        ActionName: string option
+        ActionValue: string option
+    }
+    override this.ToString() = 
+        String.Format("""{{ ChannelId = {0}, UserId = {1}, ActionName = {2}, ActionValue = {3} }}""", this.ChannelId, this.UserId, this.ActionName, this.ActionValue)
+
+    static member FromHttpContext (ctx: HttpContext) = 
+        let get key = 
+            match ctx.request.formData key with
+            | Choice1Of2 x -> Some(x)
+            | _ -> None
+
+        logSimple "SlackButtonRequest"
+        logSimple ((ctx.request.form |> Map.ofList).ToString())
+
+        let extractUser pl =
+            let j = JsonValue.Parse(pl)
+            j.TryGetProperty("user") |> Option.bind(fun urec -> urec.TryGetProperty("id")) |> Option.map(fun idOpt -> idOpt.AsString())
+        let extractChannel pl =
+            let j = JsonValue.Parse(pl)
+            j.TryGetProperty("channel") |> Option.bind(fun crec -> crec.TryGetProperty("id")) |> Option.map(fun idOpt -> idOpt.AsString())
+        let extractActionNameAndValue pl = 
+            let j = JsonValue.Parse(pl)
+            let firstAction = 
+                j.TryGetProperty("actions") 
+                |> Option.bind(fun aa -> aa.AsArray() |> Array.tryHead) 
+
+            let actionNameOpt = 
+                firstAction
+                |> Option.bind(fun a -> a.TryGetProperty("name"))
+                |> Option.map (fun n -> n.AsString())
+
+            let actionValueOpt = 
+                firstAction
+                |> Option.bind(fun fo -> fo.TryGetProperty("value"))
+                |> Option.map(fun v -> v.AsString())
+
+            (actionNameOpt, actionValueOpt)
+
+        let (user, channel, (actionName, actionValue)) = 
+            match get "payload" with
+            | Some(pl) -> (extractUser pl, extractChannel pl, extractActionNameAndValue pl)
+            | _ -> (None, None, (None, None))
+
+        {
+            ChannelId = channel
+            UserId = user
+            ActionValue = actionValue
+            ActionName = actionName
+        }
+
 type SlackResponseType = 
     | Ephemeral
     | InChannel
@@ -68,6 +131,43 @@ type SlackResponse =
         override this.ToString() = 
             "{ \"response_type\": \"" + this.responseType.ToString() + "\", \"text\": \"" + this.text + "\"}"
 
+type SlackInteractiveAction = 
+    {
+        name: string
+        text: string
+        value: string
+    }
+    with
+        override this.ToString() = 
+            String.Format("""{{ "name": "{0}", "text": "{1}", "type": "button", "value": {2} }}""", this.name, this.text, this.value)
+
+type SlackInteractiveResponse = 
+    {
+        text: string
+        buttonPrompt: string
+        fallback: string
+        callbackId: string
+        color: string
+        actions: SlackInteractiveAction array
+    }
+    with
+        override this.ToString() = 
+            logSimple "SlackInteractiveResponse.ToString(): before attachmentsStrings"
+            let attachmentsStrings = 
+                this.actions 
+                |> Array.map(fun a -> a.ToString())
+
+            logSimple "SlackInteractiveResponse.ToString(): before attachments"
+            let attachmentFormatString = 
+                """{{ "text": "", "attachment_type": "default", "callback_id": "0", "actions": [{0}] }}"""
+            let attachments = 
+                String.Format(attachmentFormatString, String.Join(",", attachmentsStrings))
+
+            logSimple "SlackInteractiveResponse.ToString(): before String.Format"
+            let res = String.Format("{{ \"text\": \"{0}\", \"attachments\": [{1}]}}", this.text, attachments)
+            logSimple("SlackInteractiveResponse.ToString(): res is\n" + res)
+            res
+
 type State = 
     {
         conversations: ConcurrentBag<Conversation>
@@ -75,21 +175,43 @@ type State =
         incomingWebhook: string
     }
 
+let buildInteractiveResponse(choices: string seq) = 
+    {
+        SlackInteractiveResponse.text = "choice"
+        buttonPrompt = "Make your choice"
+        fallback = "None"
+        callbackId = "none"
+        color = "#7F7F7F"
+        actions = choices |> Seq.mapi (fun i c -> { SlackInteractiveAction.name = i.ToString(); text = c; value = i.ToString() }) |> Array.ofSeq
+    }
+
 let advanceComputerSpeakingIfNecessary(state: State, d: Dialogue, channel: string): unit = 
     // if the dialogue is in the ComputerSpeaking state, set up an async handler to advance it to the
     // next state after a moment.
     match d.s with
     | DialogueState.ComputerSpeaking -> 
+        logSimple "in advanceComputerSpeakingIfNecessary, DialogueState.ComputerSpeaking branch"
         async {
+            logSimple "in async handler, before sleep"
             do! Async.Sleep(1000)
-            let (newDialogue2, result2) = runDialogue(d, "")
+            logSimple "in async handler, after sleep"
+            let (newDialogue2, result2, choices2) = runDialogue(d, "")
+            logSimple "in async handler, after runDialogue"
             state.activeDialogues.TryUpdate(channel, newDialogue2, d) |> ignore
+            logSimple "in async handler, after TryUpdate"
 
             // send the request to the webhook
-            let response = "{\"text\": \"" + result2 + "\"}"
-            Http.RequestString(state.incomingWebhook, httpMethod = "POST", body = TextRequest(response), headers = [ HttpRequestHeaders.ContentType(HttpContentTypes.Json) ]) |> ignore
+            let response = buildInteractiveResponse(choices2)
+            logSimple "in async handler, after buildInteractiveResponse"
+            logSimple(response.ToString())
+            logSimple "in async handler, after logging response.ToString()"
+            //let response = "{\"text\": \"" + result2 + "\"}"
+            Console.WriteLine(response.ToString())
+            Http.RequestString(state.incomingWebhook, httpMethod = "POST", body = TextRequest(response.ToString()), headers = [ HttpRequestHeaders.ContentType(HttpContentTypes.Json) ]) |> ignore
         } |> Async.StartAsTask |> ignore
-    | _ -> ()    
+    | _ ->
+        logSimple "in advanceComputerSpeakingIfNecessary, catch-all branch"
+        ()    
 
 let newDialogueHandler(state: State, name: string, channel: string, uid: string) = 
     match (state.conversations |> Seq.tryFind (fun c -> c.name = name)) with
@@ -109,7 +231,7 @@ let newDialogueHandler(state: State, name: string, channel: string, uid: string)
             // start the dialogue automatically after 1 second
             async {
                 do! Async.Sleep(1000)
-                let (newDialogue, result) = runDialogue(d, "")
+                let (newDialogue, result, choices) = runDialogue(d, "")
 
                 if (not s) then 
                     state.activeDialogues.TryAdd(channel, newDialogue) |> ignore
@@ -118,7 +240,7 @@ let newDialogueHandler(state: State, name: string, channel: string, uid: string)
 
                 // send the request to the webhook
                 let response = "{\"text\": \"" + result + "\"}"
-                Http.RequestString(state.incomingWebhook, httpMethod = "POST", body = TextRequest(response), headers = [ HttpRequestHeaders.ContentType(HttpContentTypes.Json) ]) |> ignore
+                Http.RequestString(state.incomingWebhook, httpMethod = "POST", body = TextRequest(response.ToString()), headers = [ HttpRequestHeaders.ContentType(HttpContentTypes.Json) ]) |> ignore
 
                 do! Async.Sleep(1000)
                 advanceComputerSpeakingIfNecessary(state, newDialogue, channel)                
@@ -135,15 +257,27 @@ let dialogueRequestHandler(state: State)(r: SlackRequest): SlackResponse =
         newDialogueHandler(state, dialogueName, channel, userId)
     | (Some(cmd :: tail), Some(channel), Some(_)) when cmd = "run" ->
         let d = state.activeDialogues.Item(channel)
-        let (newDialogue, result) = runDialogue(d, String.Join(" ", tail))
+        let (newDialogue, result, choices) = runDialogue(d, String.Join(" ", tail))
         state.activeDialogues.TryUpdate(channel, newDialogue, d) |> ignore
-
         advanceComputerSpeakingIfNecessary(state, newDialogue, channel)
         { SlackResponse.responseType = InChannel; text = result }
     | _ -> { SlackResponse.responseType = Ephemeral; text = "Unrecognized command or something else failed" }
 
+let buttonRequestHandler(state: State)(r: SlackButtonRequest): SlackResponse = 
+    let cmdOpt = Some(["run"; r.ActionValue |> Option.orDefault("")])
+    logSimple("buttonRequestHandler(): r = " + r.ToString())
+    match (cmdOpt, r.ChannelId, r.UserId) with
+    | (Some(cmd :: tail), Some(channel), Some(_)) when cmd = "run" ->
+        let d = state.activeDialogues.Item(channel)
+        let (newDialogue, result, choices) = runDialogue(d, String.Join(" ", tail))
+        state.activeDialogues.TryUpdate(channel, newDialogue, d) |> ignore
+        advanceComputerSpeakingIfNecessary(state, newDialogue, channel)
+        { SlackResponse.responseType = InChannel; text = result }
+    | _ -> { SlackResponse.responseType = Ephemeral; text = "Unrecognized button action" }
+
 let dialogueHandler(state: State)(ctx: HttpContext) = 
     (
+        logSimple "in dialogueHandler"
         let resp = 
             SlackRequest.FromHttpContext ctx
             |> (dialogueRequestHandler(state))
@@ -152,8 +286,19 @@ let dialogueHandler(state: State)(ctx: HttpContext) =
         |> OK
     ) ctx
 
+let buttonHandler(state: State)(ctx: HttpContext) = 
+    (
+        let resp = 
+            SlackButtonRequest.FromHttpContext ctx
+            |> (buttonRequestHandler(state))
+
+        resp.ToString()
+        |> OK
+    ) ctx
+
 let echoHandler(ctx: HttpContext) = 
     (
+        logSimple "in echoHandler"
         SlackRequest.FromHttpContext ctx
         |> fun req ->
             let resp = 
@@ -167,8 +312,10 @@ let echoHandler(ctx: HttpContext) =
 
 let app(state: State) = 
     let handler = dialogueHandler(state)
+    let bh = buttonHandler(state)
     choose [
         POST >=> path "/echo" >=> handler >=> Writers.setMimeType "application/json"
+        POST >=> path "/button" >=> bh >=> Writers.setMimeType "application/json"
     ]
 
 //////////////////////////////
